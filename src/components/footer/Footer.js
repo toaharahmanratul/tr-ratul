13d6acade2eb73436724f50c9aa373c8650fa6bc
Toaha Rahman Ratul <toaharahman.ratul@gmail.com>
2025-02-12 23:22:06 +0600

    Initiated the project - working on the layout

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = ({}) => {
+  return <div className={styles["wrapper"]}></div>;
+};
+
+export default Footer;
---
+++ src/sections/home-page/HomePageSection.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./HomePageSection.module.css";
+
+const HomePageSection = () => {
+  return <div>HomePage</div>;
+};
+
+export default HomePageSection;
